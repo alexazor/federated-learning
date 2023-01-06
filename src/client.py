1189f8e68ca72034b8417994fd74c86c53ae18e5
Franck XU <akubrain@MacBook-Pro-de-Franck.local>
2023-01-06 11:25:48 +0100

    Adding some methods to Client class

--- src/client.py
+++ src/client.py
@@ -1,27 +1,46 @@
-import numpy as np
-from utils import encrypt_vector
+from numpy._typing import NDArray
+from utils import encrypt_matrix, sum_encrypted_matrix
 
 
 class Client:
-    def __init__(self, id, pubkey, model, data, target: NDArray, encrypt):
-
+    def __init__(self, id, pubkey, model, data, target: NDArray, encrypt, nb_iter=100):
         self.id = id
         self.pubkey = pubkey
         self.model = model
         self.data = data
         self.target = target
         self.encrypt = encrypt  # encrypt function
+        self.nb_iter = nb_iter
 
     def local_fit(self):
-        for _ in range(self.n_iter):
-            gradient = self.compute_gradient()
-            self.gradient_step(gradient)
+        for _ in range(self.nb_iter):
+            gradients = self.compute_gradient()
+            self.model.update_model(gradients)
+            print(self.model.compute_loss(self.data, self.target))
+        pass
 
-    def gradient_step(self, gradient):
-        self.weights -= self.lr * gradient
+    def gradient_step(self, gradients):
+        self.model.update_model(gradients)
+        pass
 
-    def encrypt_gradient(self):
+    def compute_gradient(self):
+        return self.model.compute_gradient(self.data, self.target)
 
-        encrypted_gradient = encrypt_vector(
-            self.pubkey, self.compute_gradient(), self.encrypt)
-        return encrypted_gradient
+    def predict(self, X: NDArray) -> float:
+        """Score test data"""
+        return self.model.predict(X)
+
+    def encrypted_gradient(self, sum_to=None) -> list[NDArray]:
+        """Compute gradient. Encrypt it.
+        When `sum_to` is given, sum the encrypted gradient to it, assumed
+        to be another vector of the same size
+        """
+        gradients = [encrypt_matrix(self.pubkey, gradient, self.encrypt)
+                     for gradient in self.compute_gradient()]
+
+        if sum_to is not None:
+            if len(sum_to) != len(gradients):
+                raise Exception('Encrypted vectors must have the same size')
+            return [sum_encrypted_matrix(grad_sum_to, grad) for grad_sum_to, grad in zip(sum_to, gradients)]
+        else:
+            return gradients
